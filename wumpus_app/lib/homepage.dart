b088846f9841f0471c9ff9fc7fbb8ec7440a31c9
hahalinh <linhdamemail@gmail.com>
2022-12-05 20:52:25 -0500

    add Shoôt buttons and recenter

--- wumpus_app/lib/homepage.dart
+++ wumpus_app/lib/homepage.dart
@@ -69,16 +69,16 @@ class HomePageState extends State<HomePage> {
                               // current index go back to moved index
                               if (playerPosition == index) {
                                 return MyPixel(
-                                    color: Color.fromARGB(255, 247, 41, 27));
+                                    color: Color.fromARGB(255, 250, 22, 5));
                               } else {
                                 // current index go to grey (never moved before area)
                                 return MyPixel(
-                                    color: Color.fromARGB(255, 247, 190, 185));
+                                    color: Color.fromARGB(255, 255, 188, 182));
                               }
                             } else if (playerPosition == index) {
                               moved.add(playerPosition);
                               return MyPixel(
-                                  color: Color.fromARGB(255, 247, 41, 27));
+                                  color: Color.fromARGB(255, 250, 22, 5));
                             } else {
                               return MyPixel(color: Colors.white);
                             }
@@ -87,71 +87,125 @@ class HomePageState extends State<HomePage> {
                   ),
                   Expanded(
                     child: Container(
-                      child: Row(children: [
-                        // ignore: prefer_const_constructors
-                        FittedBox(
-                          fit: BoxFit.fitWidth,
-                          //Backend: update status
-                          child: Text(
-                            "Status update",
-                            style: TextStyle(fontSize: 36),
-                          ),
-                        ),
-                        Spacer(),
-                        Row(
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Row(
+                            // ignore: prefer_const_constructors
+                            FittedBox(
+                              fit: BoxFit.fitWidth,
+                              //Backend: update status
+                              child: Text(
+                                "Status update",
+                                style: TextStyle(fontSize: 36),
+                              ),
+                            ),
+
+                            Row(children: [
+                              Container(
+                                child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
                                     children: [
-                                      MyButton(),
-                                      MyButton(
-                                          function: moveUp,
-                                          color: Colors.grey,
-                                          child: Icon(
-                                            Icons.arrow_drop_up,
-                                            size: 50,
-                                          )),
-                                      MyButton(),
-                                    ],
-                                  ),
-                                  Row(
+                                      Row(
+                                        children: [
+                                          MyButton(),
+                                          MyButton(
+                                              function: moveUp,
+                                              color: Colors.grey,
+                                              child: Icon(
+                                                Icons.arrow_drop_up,
+                                                size: 50,
+                                              )),
+                                          MyButton(),
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          MyButton(
+                                              function: moveLeft,
+                                              color: Colors.grey,
+                                              child: Icon(
+                                                Icons.arrow_left,
+                                                size: 50,
+                                              )),
+                                          MyButton(),
+                                          MyButton(
+                                              function: moveRight,
+                                              color: Colors.grey,
+                                              child: Icon(
+                                                Icons.arrow_right,
+                                                size: 50,
+                                              )),
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          MyButton(),
+                                          MyButton(
+                                              function: moveDown,
+                                              color: Colors.grey,
+                                              child: Icon(
+                                                Icons.arrow_drop_down,
+                                                size: 50,
+                                              )),
+                                          MyButton(),
+                                        ],
+                                      )
+                                    ]),
+                              ),
+                              Spacer(),
+                              Container(
+                                child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
                                     children: [
-                                      MyButton(
-                                          function: moveLeft,
-                                          color: Colors.grey,
-                                          child: Icon(
-                                            Icons.arrow_left,
-                                            size: 50,
-                                          )),
-                                      MyButton(),
-                                      MyButton(
-                                          function: moveRight,
-                                          color: Colors.grey,
-                                          child: Icon(
-                                            Icons.arrow_right,
-                                            size: 50,
-                                          )),
-                                    ],
-                                  ),
-                                  Row(
-                                    children: [
-                                      MyButton(),
-                                      MyButton(
-                                          function: moveDown,
-                                          color: Colors.grey,
-                                          child: Icon(
-                                            Icons.arrow_drop_down,
-                                            size: 50,
-                                          )),
-                                      MyButton(),
-                                    ],
-                                  )
-                                ])
-                          ],
-                        ),
-                      ]),
+                                      Row(
+                                        children: [
+                                          MyButton(),
+                                          MyButton(
+                                              color: Colors.grey,
+                                              child: Icon(
+                                                Icons.arrow_drop_up,
+                                                size: 50,
+                                              )),
+                                          MyButton(),
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          MyButton(
+                                              color: Colors.grey,
+                                              child: Icon(
+                                                Icons.arrow_left,
+                                                size: 50,
+                                              )),
+                                          MyButton(),
+                                          MyButton(
+                                              color: Colors.grey,
+                                              child: Icon(
+                                                Icons.arrow_right,
+                                                size: 50,
+                                              )),
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          MyButton(),
+                                          MyButton(
+                                              color: Colors.grey,
+                                              child: Icon(
+                                                Icons.arrow_drop_down,
+                                                size: 50,
+                                              )),
+                                          MyButton(),
+                                        ],
+                                      )
+                                    ]),
+                              ),
+                            ]),
+                          ]),
                     ),
                   ),
                 ],
